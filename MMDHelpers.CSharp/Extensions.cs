e69ce6c28f1e3530d177d2a4682e8eca19f875cf
Antonio Marcos de Souza Santos Junior <antonio.santos@neon.com.br>
2020-12-16 13:22:55 -0300

    initial commit

---
+++ MMDHelpers.CSharp/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace MMDHelpers.CSharp.Extensions
+{
+    public static class Extensions
+    {
+        public static string ToCurrentPath(this string path) => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), path);
+    }
+}
