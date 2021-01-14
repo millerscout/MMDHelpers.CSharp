a17f407eb4b777732b86bff722c771d314842753
Antonio Marcos de Souza Santos Junior <antonio.santos@neon.com.br>
2021-01-14 19:16:50 -0300

    wip

---
+++ MMDHelpers.CSharp.LDAP/LDAPService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace MMDHelpers.CSharp.LDAP
+{
+    public class LDAPService
+    {
+        private string path;
+
+        public LDAPService(string Path)
+        {
+            this.path = Path;
+        }
+
+        public bool login(string user, string password)
+        {
+
+            try
+            {
+                DirectoryEntry directoryEntry = new DirectoryEntry(path, user, password);
+                DirectorySearcher directorySearcher = new DirectorySearcher(directoryEntry);
+                directorySearcher.Filter = "(SAMAccountName=" + user + ")";
+                SearchResult searchResult = directorySearcher.FindOne();
+                if ((Int32)searchResult.Properties["userAccountControl"][0] == 512)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- MMDHelpers.CSharp.LocalData/SQLiteService.cs
+++ MMDHelpers.CSharp.LocalData/SQLiteService.cs
@@ -17,6 +17,7 @@ namespace MMDHelpers.CSharp.LocalData
         public static void Setup(string DBLocation = null, List<string> commands = null)
         {
             DBLocation = string.IsNullOrWhiteSpace(DBLocation) ? "cachedb.sqlite".ToCurrentPath() : DBLocation;
+            DbLocation = DBLocation;
             connectionstring = $"Data Source={DbLocation}; Version=3;";
             if (!File.Exists(DbLocation))
             {
@@ -48,20 +49,68 @@ namespace MMDHelpers.CSharp.LocalData
             }
         }
 
-        public IEnumerable<T> Query<T>(string query, object param = null)
+        public IEnumerable<T> Query<T>(string query, object param = null, int timeout = 0)
         {
             using (var con = new SQLiteConnection(connectionstring))
+            {
+                if (timeout > 0) con.DefaultTimeout = timeout;
                 return con.Query<T>(query, param);
+            }
         }
-        public T FirstOrDefault<T>(string query, object param = null)
+        public T FirstOrDefault<T>(string query, object param = null, int timeout = 0)
         {
             using (var con = new SQLiteConnection(connectionstring))
+            {
+                if (timeout > 0) con.DefaultTimeout = timeout;
                 return con.QueryFirstOrDefault<T>(query, param);
+            }
         }
-        public bool Execute(string query, object param = null)
+        public bool Execute(string query, object param = null, int timeout = 0)
         {
             using (var con = new SQLiteConnection(connectionstring))
+            {
+                if (timeout > 0) con.DefaultTimeout = timeout;
                 return con.Execute(query, param) == 1;
+            }
+        }
+
+        public int InsertBatch(string query, IEnumerable<Dictionary<string, object>> items, int timeout = 0)
+        {
+
+            var executed = 0;
+
+            if (items.Count() == 0 || items.Sum(c => c.Count) == 0) return 0;
+
+            using (var connection = new SQLiteConnection(connectionstring))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                using (var command = connection.CreateCommand())
+                {
+                    command.Prepare();
+                    command.CommandText = query;
+
+                    foreach (var item in items)
+                    {
+                        command.Parameters.Clear();
+                        foreach (var dict in item)
+                        {
+
+                            var param = command.CreateParameter();
+                            param.ParameterName = dict.Key;
+                            param.Value = dict.Value;
+                            command.Parameters.Add(param);
+                        }
+                        if (timeout > 0) command.CommandTimeout = timeout;
+
+                        executed += command.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+                }
+                connection.Close();
+            }
+            return executed;
         }
     }
 }
