5df034e9e26c9765b9da3f7b12e632f1443fa89e
Marcos Miller Dantas <mmillerdantas@outlook.com>
2021-01-24 20:11:49 -0300

    Added Extension to write to file

--- MMDHelpers.CSharp/Extensions.cs
+++ MMDHelpers.CSharp/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -7,5 +8,17 @@ namespace MMDHelpers.CSharp.Extensions
     public static class Extensions
     {
         public static string ToCurrentPath(this string path) => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), path);
+        public static void WriteToFile(this string Path, List<string> logs)
+        {
+            using (FileStream file = new FileStream(Path, FileMode.Append, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                foreach (var message in logs)
+                {
+                    sw.WriteLine(message);
+                }
+
+            }
+        }
     }
 }
