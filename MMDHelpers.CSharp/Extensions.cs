9b104c8d6c30df734b1d6234a7ad3ffad02694be
Antonio Santos <antonio.santos@neon.com.br>
2021-02-15 10:25:06 -0300

    added ienumerable serialization

--- MMDHelpers.CSharp/Extensions.cs
+++ MMDHelpers.CSharp/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.IO;
 using System.Reflection;
 
@@ -19,6 +21,27 @@ namespace MMDHelpers.CSharp.Extensions
                 }
 
             }
+        }
+        /// <summary>
+        /// Helper to transform an IEnumerable to Datatable, Useful to Bulk insert 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static DataTable AsDataTable<T>(this IEnumerable<T> data)
+        {
+            var properties = TypeDescriptor.GetProperties(typeof(T));
+            var table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
         }
     }
 }
