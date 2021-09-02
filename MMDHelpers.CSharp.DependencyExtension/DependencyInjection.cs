f437d7f69fa2598ded65266b7b84de78a211899b
Marcos Miller Dantas <mmillerdantas@outlook.com>
2021-09-01 23:47:20 -0300

    MMD orchestration initial commit

---
+++ MMDHelpers.CSharp.DependencyExtension/DependencyInjection.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+
+namespace MMDHelpers.CSharp.DependencyInjection.Serilog
+{
+
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Service to send/receive commands to configured application.
+        /// </summary>
+        /// <param name="service"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddOrchestrationControl(this IServiceCollection service, config t)
+        {
+            service.AddSingleton<MyClass>();
+
+            return service;
+        }
+    }
+    public static class MMDSerilogOrchestration
+    {
+        public static LoggerConfiguration MMDSink(this LoggerSinkConfiguration sinkConfiguration)
+        {
+            return sinkConfiguration.Sink(new MMDSink());
+        }
+
+    }
+
+    public class MMDSink : ILogEventSink
+    {
+        public MMDSink()
+        {
+
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            System.Console.WriteLine(logEvent.RenderMessage());
+        }
+    }
+    public class MyClass
+
+    {
+        public MyClass(config s)
+        {
+
+        }
+       
+    }
+    public class config
+    {
+
+    }
+
+}
---
+++ ServiceOrchestration/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MMDHelpers.CSharp.DependencyInjection;
+using MMDHelpers.CSharp.DependencyInjection.Serilog;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using System;
+
+namespace ServiceOrchestration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+           .MinimumLevel.Information()
+           .WriteTo.MMDSink()
+           .CreateLogger();
+
+
+            Log.Information("Processed {@Position} in {Elapsed} ms.", "23132", 123);
+
+            Log.CloseAndFlush();
+
+            IServiceCollection services = new ServiceCollection();
+            services.AddOrchestrationControl(new config());
+        }
+
+    }
+   
+}
